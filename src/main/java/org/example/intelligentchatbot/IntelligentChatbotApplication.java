ab172d787782729bd09792fc8fcdbaac62480414
phoenix-cities <3299308759@qq.com>
2025-12-14 13:06:12 +0800

    init

---
+++ src/main/java/org/example/intelligentchatbot/IntelligentChatbotApplication.java
@@ -0,0 +1,13 @@
+package org.example.intelligentchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/intelligentchatbot/IntelligentChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.intelligentchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
