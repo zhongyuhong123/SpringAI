c527cbb297a656f4e9babcccb0f3cb0da0bdc01a
phoenix-cities <3299308759@qq.com>
2025-12-14 13:50:05 +0800

    init

--- src/main/java/org/example/intelligentchatbot/IntelligentChatbotApplication.java
+++ src/main/java/org/example/intelligentchatbot/IntelligentChatbotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IntelligentChatbotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IntelligentChatbotApplication.class, args);
 	}
-
 }
